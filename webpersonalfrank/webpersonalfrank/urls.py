7c3c282ad5f70f5968ff95d6e7a50c2ca9ea8055
frankiyonki <llonchfrank@gmail.com>
2024-11-21 18:37:40 +0100

    uhhh
    hhh

---
+++ webpersonalfrank/webpersonalfrank/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about-me/', views.about, name="about"),
+    path('portfolio/', views.portfolio, name="portfolio"),
+    path('contact/', views.contact, name="contact"),
+    path('admin/', admin.site.urls),
+    path('portfolio/<int:portfolio_id>/', views.portfolio, name="portfolio"),
+]
